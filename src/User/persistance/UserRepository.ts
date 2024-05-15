2b767ffe8d6f92109aabdb00f1ba32b7ea579b2f
folmos@confluence.com <folmos@confluence.com>
2024-05-15 22:40:57 +0200

    User admin password

--- src/User/persistance/UserRepository.ts
+++ src/User/persistance/UserRepository.ts
@@ -2,12 +2,11 @@ import { Repository } from "../../shared/persistence/Repository";
 import { IUserRepository } from "./interfaces/IUserRepository";
 import { User } from "./model/User";
 
-export class UserRepository extends Repository<User> implements IUserRepository
-{
+export class UserRepository extends Repository<User> implements IUserRepository {
     async seed() {
-       User.initModel(this._database._client);
-       await User.sync();
-       await User.create({ userName : 'super.admin', password:'$2y$10$ENT0.jA3ANkGjzYDaz.O0u6xhcAP6wgFAGD7G.hzWEfKR/OtTsMmO'});
+        User.initModel(this._database._client);
+        await User.sync();
+        await User.create({ userName: 'super.admin', password: '$2a$10$tiGbjYFgtAVLsqmbjS9amO0hg0.itWAnQ3eE/4OaxBRFftm11SRWq' });
     }
 
     async initModel(): Promise<void> {
@@ -16,12 +15,12 @@ export class UserRepository extends Repository<User> implements IUserRepository
 
     login(userName: string, password: string): Promise<User | null> {
         return (this._database._client.models['User']
-        .findOne({
-            where: {
-                userName,
-                password
-            }
-        }) as unknown) as Promise<User | null>;
+            .findOne({
+                where: {
+                    userName,
+                    password
+                }
+            }) as unknown) as Promise<User | null>;
     }
 
 }
